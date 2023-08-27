2602c0876eb4a5c484cfa8f4c66a566d290dbb63
Monwer, Tarek <tareqmonwer137@gmail.com>
2023-08-28 00:09:01 +0600

    Adapter: ObjectAdapter & ClassAdapter

---
+++ Behavioral/ClassAdapter/mock_customers.py
@@ -0,0 +1,8 @@
+from customer import Customer
+
+
+MOCK_CUSTOMERS = (
+    Customer("Ahmed", "Nikunja 1, Dhaka"),
+    Customer("Shahariar", "Mirpur, Dhaka"),
+    Customer("Jahid", "London, United Kingdom")
+)
---
+++ Behavioral/ClassAdapter/mock_vendors.py
@@ -0,0 +1,9 @@
+from Behavioral.PostAdapter.vendor_adapter import VendorAdapter
+from vendor import Vendor
+
+
+MOCK_VENDORS = (
+    VendorAdapter(Vendor("Daraz", 1, 1209)),
+    VendorAdapter(Vendor("Alibaba", 2, 2323)),
+    VendorAdapter(Vendor("Amazon", 3, 3534)),
+)
